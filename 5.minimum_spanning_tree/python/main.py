504bb7b0e6ee1d74793017c985f3560eeeb35f02
Youngchae James Chee <litcoderr@gmail.com>
2021-05-16 19:44:05 +0900

    main: finish python version of minimum spanning tree

--- 5.minimum_spanning_tree/python/main.py
+++ 5.minimum_spanning_tree/python/main.py
@@ -1,9 +1,39 @@
 import argparse
+from queue import PriorityQueue
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_file")
 args = parser.parse_args()
 
+class Vertex:
+    def __init__(self, index):
+        self.index = index
+
+        self.group = index
+        self.child = []
+
+class Edge:
+    def __init__(self, v1, v2, weight):
+        self.v1 = v1
+        self.v2 = v2
+        self.weight = weight
+    
+    def __lt__(self, other):
+        return self.weight < other.weight
+
 if __name__ == "__main__":
     with open(args.input_file, "r") as f:
         N = int(f.readline())
+
+        vertices = [Vertex(i) for i in range(N+1)]
+        edges = PriorityQueue()
+
+        for line in f:
+            v1, v2, weight = tuple(map(lambda x: int(x), line.split(" ")))
+            
+            edges.put(Edge(vertices[v1], vertices[v2], weight))
+
+    while not edges.empty():
+        edge = edges.get()
+
+
