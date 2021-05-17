13dbd4793ccb39e6c2e2f86a0cfa7fa273559e47
Youngchae James Chee <litcoderr@gmail.com>
2021-05-17 19:55:43 +0900

    main: finished minimum-spanning-tree in python

--- 5.minimum_spanning_tree/python/main.py
+++ 5.minimum_spanning_tree/python/main.py
@@ -9,7 +9,7 @@ class Vertex:
     def __init__(self, index):
         self.index = index
 
-        self.group = index
+        self.group = self
         self.child = []
 
 class Edge:
@@ -20,6 +20,28 @@ class Edge:
     
     def __lt__(self, other):
         return self.weight < other.weight
+
+def merge(v1: Vertex, v2: Vertex):
+    if v1.group != v2.group:
+        if len(v1.group.child) > len(v2.group.child):
+            merge_(v1.group, v2)
+        else:
+            merge_(v2.group, v1)
+        return True
+    else:
+        return False
+
+def merge_(parent: Vertex, v2: Vertex):
+    """
+    merge v2 and v2's sub group to parent
+    """
+    for child in v2.child:
+        merge_(parent, child)
+
+    v2.group = parent
+    v2.child = []
+
+    parent.child.append(v2)
 
 if __name__ == "__main__":
     with open(args.input_file, "r") as f:
@@ -33,7 +55,13 @@ if __name__ == "__main__":
             
             edges.put(Edge(vertices[v1], vertices[v2], weight))
 
+    result = []
     while not edges.empty():
         edge = edges.get()
+        success = merge(edge.v1, edge.v2)
+        
+        if success:
+            result.append(edge)
 
-
+    for edge in result:
+        print("v1: {} v2: {} weight: {}".format(edge.v1.index, edge.v2.index, edge.weight))
